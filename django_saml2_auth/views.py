65eddfc019797f14c63f79b4faa9b2437f80c156
mlachowski-mrx <112694412+mlachowski-mrx@users.noreply.github.com>
2023-06-27 13:30:49 +0200

    Update views.py

--- django_saml2_auth/views.py
+++ django_saml2_auth/views.py
@@ -142,7 +142,7 @@ def acs(request):
     try:        
         target_user = User.objects.filter(is_active=True).get(username__iexact=username)
     except User.DoesNotExist:
-        target_user = User.objects.create_user(username, email)
+        target_user = User.objects.create_user(username, nameid)
         target_user.is_active = True
         target_user.save()    
 
